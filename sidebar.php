80f4038082e77c8e5bfd53d09158a96e5138c51c
Nereaap <avilapereznerea@gmail.com>
2021-10-28 18:52:01 +0200

    ..

--- sidebar.php
+++ sidebar.php
@@ -72,5 +72,16 @@
 					</div>
 				</li>
 
+				<li class="mb-1">
+					<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+					carrito
+					</button>
+					<div class="collapse" id="dashboard-collapse">
+					<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+						<li><a href="./carrito/verCarro.php" class="link-dark rounded">Ejercicio 1</a></li>
+					</ul>
+					</div>
+				</li>
+
 				</ul>
 			</div>
